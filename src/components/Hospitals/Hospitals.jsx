499eb8daf66283bc468655f19a210064f974f8df
Sama872 <107773332+Sama872@users.noreply.github.com>
2024-07-05 19:33:38 +0300

    design for hospital page is done

--- src/components/Hospitals/Hospitals.jsx
+++ src/components/Hospitals/Hospitals.jsx
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getAllHospitals } from '../Redux/HospitalSlice';
 import { Grid } from 'react-loader-spinner';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import img1 from '../../image/hospital-animation-removebg-preview.png'
+import img2 from '../../image/hospital-bg-removebg-preview.png'
 
 
 export default function Hospitals() {
@@ -28,21 +30,29 @@ export default function Hospitals() {
           />
         </div>
       ) : (
-        <div className='row mt-5 justify-content-center'>
-          {hospital.map((hospital) => (
-            <div className="col-md-3 full-hospital-card m-2" key={hospital.id}>
-              <div className='top-hospital-card'>
-                <h4>{hospital.name}</h4>
-              </div>
-              <div className='bottom-hospital-card'>
-                <h5>{hospital.beds}</h5>
-                <h5>{hospital.phone}</h5>
-                <h5>{hospital.cases}</h5>
-                <h5>{hospital.type}</h5>
-              </div>
-            </div>
-          ))}
-        </div>
+<div className='row mt-5 justify-content-center'>
+  {hospital.map((hospital) => (
+    <div className="col-md-3 m-2" key={hospital.id}>
+      <div className='full-hospital-card'>
+          <div className='top-hospital-card'>
+           {hospital.type==='Governmental'? <img src={img1} alt='bg' className='w-75'/>:<img src={img2} alt='bg' className='w-75'/>}
+                <h6>{hospital.name}</h6>
+          </div>
+          <div className='bottom-hospital-card'>
+            <h6>Number of beds :{hospital.beds}</h6>
+            <h6>Phone Number :{hospital.phone}</h6>
+            {/* <p>{hospital.cases}</p> */}
+            <h6>Sector :{hospital.type}</h6>
+          </div>
+          <div className='hospital-bottons'>
+            <button className='btn btn-outline-success m-2'><i class="fa-regular fa-pen-to-square"></i> Edite</button>
+            <button className='btn btn-outline-danger m-2'> <i class="fa-solid fa-trash" style={{color:"red"}}></i> Delete</button>
+          </div>
+      </div>
+    </div>
+  ))}
+</div>
+
       )}
     </div>
   );
